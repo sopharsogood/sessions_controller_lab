f301cf8d6d53f470e5dfd38b85204e3400baaf18
sophie <justanothersophie@gmail.com>
2021-09-14 17:22:51 -0400

    Completed lab, all tests passing

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if session[:name]
+      redirect_to '/'
+    elsif !params[:name] || params[:name].empty?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
